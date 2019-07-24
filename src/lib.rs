f03b2946ede05fb4b7f9fa5a687ecbb120c525a2
GuoBin <844922881@qq.com>
2019-07-24 15:13:43 +0800

    http server

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+/**
+* lib
+*
+* @author GuoBin on 2019-07-24
+*/
+pub struct ThreadPool;
+
+impl ThreadPool {
+    pub fn new(size: usize) -> ThreadPool{
+        assert!(size > 0);
+        ThreadPool
+    }
+}
