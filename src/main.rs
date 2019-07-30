079d6bc6e6e4fad3565c0ff5dcd7ba2060b85b95
GuoBin <844922881@qq.com>
2019-07-30 17:09:07 +0800

    http server

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,52 @@
 // 命名空间
 use std::fs;
+use std::collections::HashMap;
 
+// 模块
 pub mod http {
     use std::net::{TcpListener, TcpStream};
     use std::io::{Read, Write};
-    use std::fs;
+    use std::{fs};
+    use std::collections::HashMap;
+    struct ThreadPool;
 
-    /// 监听tcp端口
-    pub(crate) fn listen() {
+    impl ThreadPool {
+        pub fn new(size: usize) -> ThreadPool {
+            assert!(size > 0);
+            ThreadPool{}
+        }
+        pub fn execute<F>(&self, _f: F)
+            where F: FnOnce() + Send + 'static{}
+    }
+
+    // 监听tcp端口
+    pub(crate) fn listen(asset: HashMap<&str, String>) {
+        if asset.get("index")  != None {
+            println!("{:?}", asset.get("index"));
+        }
         let listener = TcpListener::bind("127.0.0.1:7777").unwrap();
+        let pool = ThreadPool::new(4);
         for stream in listener.incoming() {
             let stream = stream.unwrap();
-            println!("Connection established!");
-            handle_connection(stream);
+            pool.execute(|| {
+                handle_connection(stream);
+            });
         }
     }
-    /// 处理stream流
+    // 处理单个的stream
     fn handle_connection(mut stream: TcpStream) {
         let mut buffer = [0; 512];
-        // 读请求
+        let get = b"GET / HTTP/1.1\r\n";
+        let _index: String = fs::read_to_string("/Users/guobin/idea/play-rust/src/index.html").unwrap();
+        let _not_found: String = fs::read_to_string("/Users/guobin/idea/play-rust/src/404.html").unwrap();
         stream.read(&mut buffer).unwrap();
         println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
-        let get = b"GET / HTTP/1.1\r\n";
-        // GET请求
         if buffer.starts_with(get) {
-            let _content = fs::read_to_string("/Users/guobin/idea/play-rust/src/index.html").unwrap();
-            let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", _content);
+            let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", _index);
             stream.write(response.as_bytes()).unwrap();
             stream.flush().unwrap();
         } else {
-            let _content = fs::read_to_string("/Users/guobin/idea/play-rust/src/404.html").unwrap();
-            let response = format!("HTTP/1.1 404 NOT FOUND\r\n\r\n{}", _content);
+            let response = format!("HTTP/1.1 404 NOT FOUND\r\n\r\n{}", _not_found);
             stream.write(response.as_bytes()).unwrap();
             stream.flush().unwrap();
         }
@@ -38,9 +54,13 @@ pub mod http {
 }
 
 fn main() {
+    println!("Binary: {:b}, Hex:{:x}, Octal: {:o}", 10, 10, 10);
     let _index: String = fs::read_to_string("/Users/guobin/idea/play-rust/src/index.html").unwrap();
     let _not_found: String = fs::read_to_string("/Users/guobin/idea/play-rust/src/404.html").unwrap();
-    println!("index:\n {}", _index);
-    http::listen();
+    let mut assets_file = HashMap::new();
+    assets_file.insert("index", _index);
+    assets_file.insert("not_found", _not_found);
+    println!("server listener on http://127.0.0.1:7777");
+    http::listen(assets_file);
 }
 
